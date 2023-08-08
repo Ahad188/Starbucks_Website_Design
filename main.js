b313d594689d06d7a234d0e991b8c86e736072be
AhaD <abuahad35188@gmail.com>
2023-08-09 00:55:12 +0600

    all ok

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log('hello world html,css javascript');   
+let menu = document.getElementById('menu-icon') ;
+let navigate = document.getElementById('navigate') ;
+ 
+
+ 
+     menu.onclick=()=>{
+          // console.log("ok menu");
+          menu.classList.toggle('bx-x')
+          navigate.classList.toggle('active') 
+     }
+     window.scroll=()=>{
+          menu.classList.remove('bx-x')
+          navigate.classList.remove('active') 
+     }
+ 
